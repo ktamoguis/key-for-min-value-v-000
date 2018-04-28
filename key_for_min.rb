0ec815bed0fa5653330560f7b2ac9eb0a32a0d0b
Karl Amoguis <ktamoguis@gmail.com>
2018-04-28 09:48:36 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if name_hash == {}
       return nil
-    elsif
+    elsif value < minvalue
       minkey = key
       minvalue = value
     end
